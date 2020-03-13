ebe5b1e5d4d3e7fa247173bca8ded2a0f091628a
unknown <raulberrios8@gmail.com>
2020-03-13 12:10:46 -0300

    Add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    SERVER_URL: 'https://devapi.purasangrecrossfit.cl',
+    SERVER_URL: 'https://api3.purasangrecrossfit.cl',
     // SERVER_URL: 'http://purasangre-api.test',
-    IMAGE_URL: 'https://desarrollo.purasangrecrossfit.cl',
+    IMAGE_URL: 'https://admin.purasangrecrossfit.cl',
     purasangreAPIKey: '6xpyMQ7zXLZwmlkdIzCyh6eupMj4pDEA0cx3cHzf'
 };
